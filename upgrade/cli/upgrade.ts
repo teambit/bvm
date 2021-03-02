39d65c7275bbc20a8db53866ea37a3e35e5cfad7
Gilad Shoham <shoham.gilad@gmail.com>
2021-03-02 11:43:39 +0200

    fix upgrade description

--- upgrade/cli/upgrade.ts
+++ upgrade/cli/upgrade.ts
@@ -2,17 +2,17 @@ import type {CommandModule, Argv} from 'yargs';
 
 export class UpgradeCmd implements CommandModule {
   aliases = ['u', 'upgrade'];
-  describe = 'install latest bit version from the server and delete the previous installed version';
+  describe = 'install latest bit version from the server, update the current version, and delete the previous installed version';
   command = [
-    'list'
+    'upgrade'
   ];
   builder(yargs: Argv) {
     yargs
-    .example('$0 upgrade', 'install latest bit version from the server and delete the previous installed version')
+    .example('$0 upgrade', 'install latest bit version from the server, update the current version, and delete the previous installed version')
     return yargs;
   }
   handler(args) {
-    return console.log(`run list command with remote ${args.remote}`);
+    return console.log(`run upgrade command`);
   };
 }
 
