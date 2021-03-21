e1c0d450f5458f6b8fd8f6214b83ac059a980777
Gilad Shoham <shoham.gilad@gmail.com>
2021-03-21 12:42:33 +0200

    do not use bvm error on file downloader

--- teambit.toolbox/network/progress-bar-file-downloader/progress-bar-file-downloader.ts
+++ teambit.toolbox/network/progress-bar-file-downloader/progress-bar-file-downloader.ts
@@ -5,7 +5,6 @@ import Progress from 'node-fetch-progress';
 import cliProgress from 'cli-progress';
 import util from 'util';
 import { pipeline } from 'stream';
-import { BvmError } from '@teambit/bvm.error';
 
 const streamPipeline = util.promisify(pipeline);
 
@@ -43,7 +42,7 @@ export async function download(url: string, destination: string, opts: DownloadO
   const finalOpts = Object.assign({}, defaults, opts);
   const exists = await fs.pathExists(destination);
   if (exists && !finalOpts.overrideExisting) {
-    throw new BvmError(`path ${destination} already exists`);
+    throw new Error(`path ${destination} already exists`);
   }
   const dirname = path.dirname(destination);
   if (finalOpts.ensureDir) {
@@ -54,7 +53,7 @@ export async function download(url: string, destination: string, opts: DownloadO
 
   bar.start(1, 0, { speed: 'N/A' });
   progress.on('progress', (p: any) => bar.update(p.progress, { speed: p.rateh, etah: p.etah }));
-  if (!response.ok) throw new BvmError(`unexpected response ${response.statusText}`);
+  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
   await streamPipeline(response.body, fs.createWriteStream(destination));
   return;
 }
