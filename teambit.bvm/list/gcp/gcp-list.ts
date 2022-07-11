a99bd49d3d39b064e673baf691e9a6fc8a02b867
Zoltan Kochan <z@kochan.io>
2022-07-11 16:54:26 +0300

    fix: fetch using proxy settings

--- teambit.bvm/list/gcp/gcp-list.ts
+++ teambit.bvm/list/gcp/gcp-list.ts
@@ -1,9 +1,11 @@
 import { GcpStorage } from '@teambit/gcp.storage';
+import { getAgent } from '@teambit/toolbox.network.agent';
 import fetch from 'node-fetch';
 import { GcpVersion } from './gcp-version';
 import { RemoteVersionList } from '../version-list';
 import { RemoteVersion } from '../version';
 
+const BIT_INDEX_JSON = 'https://bvm.bit.dev/bit/index.json';
 const bucketName = 'bvm.bit.dev';
 const prefix = 'versions';
 
@@ -20,11 +22,17 @@ export type Release = {
 } & Partial<Record<ReleaseType, true>>
 
 export class GcpList {
-  constructor(private gcpStorage: GcpStorage, private osType = 'Darwin', private arch = 'x64', private releaseType: ReleaseType = ReleaseType.NIGHTLY_FROM_OLD_LOCATION) { }
+  constructor(
+    private gcpStorage: GcpStorage,
+    private proxyConfig = {},
+    private osType = 'Darwin',
+    private arch = 'x64',
+    private releaseType: ReleaseType = ReleaseType.NIGHTLY_FROM_OLD_LOCATION,
+  ) { }
 
   async list(): Promise<RemoteVersionList> {
     if (this.releaseType !== ReleaseType.NIGHTLY_FROM_OLD_LOCATION) {
-      const releases = await fetchReleasesList();
+      const releases = await this._fetchReleasesList();
       const remoteVersions = releases
         .filter((release) => release[this.releaseType] === true)
         .map((release) => this._createRemoteVersion(release));
@@ -36,6 +44,13 @@ export class GcpList {
       return gcpVersion.toRemoteVersion();
     });
     return new RemoteVersionList(remoteVersions);
+  }
+
+  async _fetchReleasesList(): Promise<Release[]> {
+    const response = await fetch(BIT_INDEX_JSON, {
+      agent: getAgent(BIT_INDEX_JSON, this.proxyConfig),
+    });
+    return await response.json() as Release[];
   }
 
   _createRemoteVersion(release: Release): RemoteVersion {
@@ -55,13 +70,8 @@ export class GcpList {
 
   static create(releaseType: ReleaseType = ReleaseType.NIGHTLY_FROM_OLD_LOCATION, osType = 'Darwin', arch = 'x64', proxyConfig?: {}) {
     const gcpStorage = GcpStorage.create(bucketName, proxyConfig);
-    return new GcpList(gcpStorage, osType, arch, releaseType);
+    return new GcpList(gcpStorage, proxyConfig, osType, arch, releaseType);
   }
-}
-
-async function fetchReleasesList(): Promise<Release[]> {
-  const response = await fetch('https://bvm.bit.dev/bit/index.json');
-  return await response.json() as Release[];
 }
 
 function getVersionFromFileName(fileName: string) {
