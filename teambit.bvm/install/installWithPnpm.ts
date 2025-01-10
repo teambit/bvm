892a1f30a0a13cd225fa5c03f8d37d8cec348dd7
Zoltan Kochan <z@kochan.io>
2025-01-10 17:42:42 +0100

    refactor: install with pnpm

--- teambit.bvm/install/installWithPnpm.ts
+++ teambit.bvm/install/installWithPnpm.ts
@@ -5,30 +5,9 @@ import { install } from '@pnpm/plugin-commands-installation';
 import { readWantedLockfile } from '@pnpm/lockfile.fs';
 
 export async function installWithPnpm(fetch, version: string, dest: string) {
-  const url = `https://bvm.bit.dev/bit/versions/${version}/pnpm-lock.yaml`;
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+  await fetchLockfile(fetch, version, dest);
+  await createPackageJsonFile(dest);
 
-  const lockfilePath = path.join(dest, 'pnpm-lock.yaml');
-  const fileStream = fs.createWriteStream(lockfilePath);
-
-  fs.mkdirSync(dest, { recursive: true })
-  response.body.pipe(fileStream);
-
-  await new Promise<void>((resolve, reject) => {
-    fileStream.on('finish', () => resolve());
-    fileStream.on('error', (err: Error) => reject(err));
-  });
-
-  const lockfile = await readWantedLockfile(dest, { ignoreIncompatible: false });
-  fs.writeFileSync(path.join(dest, 'package.json'), JSON.stringify({
-    dependencies: {
-      '@teambit/bit': lockfile?.importers['.'].specifiers['@teambit/bit'],
-    },
-    pnpm: {
-      overrides: lockfile?.overrides ?? {},
-    },
-  }, null, 2), 'utf8');
   const cliOptions = {
     argv: [],
     dir: dest,
@@ -48,4 +27,33 @@ export async function installWithPnpm(fetch, version: string, dest: string) {
   // pnpm is doing some actions in workers.
   // We need to finish them, when we're done.
   await global['finishWorkers']();
+}
+
+async function fetchLockfile(fetch, version: string, dest: string): Promise<void> {
+  const url = `https://bvm.bit.dev/bit/versions/${version}/pnpm-lock.yaml`;
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+
+  const lockfilePath = path.join(dest, 'pnpm-lock.yaml');
+  const fileStream = fs.createWriteStream(lockfilePath);
+
+  fs.mkdirSync(dest, { recursive: true })
+  response.body.pipe(fileStream);
+
+  await new Promise<void>((resolve, reject) => {
+    fileStream.on('finish', () => resolve());
+    fileStream.on('error', (err: Error) => reject(err));
+  });
+}
+
+async function createPackageJsonFile(dest: string) {
+  const lockfile = await readWantedLockfile(dest, { ignoreIncompatible: false });
+  fs.writeFileSync(path.join(dest, 'package.json'), JSON.stringify({
+    dependencies: {
+      '@teambit/bit': lockfile?.importers['.'].specifiers['@teambit/bit'],
+    },
+    pnpm: {
+      overrides: lockfile?.overrides ?? {},
+    },
+  }, null, 2), 'utf8');
 }
